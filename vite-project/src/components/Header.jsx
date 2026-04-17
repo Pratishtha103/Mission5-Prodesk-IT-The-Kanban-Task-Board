ad5374ec11ec0adf1c158f999ddbbe749af44c66
Pratishtha103 <11f23pratishtha@gmail.com>
2026-04-17 19:06:48 +0530

    Header Component added and App.jsa updated

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+    <div className="flex justify-center">
+        <p className="font-sans text-gray-700 text-7xl">Kanban Task Board</p>
+    </div>
+    );
+}
+export default Header
